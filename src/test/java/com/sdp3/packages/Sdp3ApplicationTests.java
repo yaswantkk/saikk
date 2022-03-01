75dd705464afd9a44f3d6c16d90b29030c9c1aff
Pradeepreddy580 <pradeep30061@gmail.com>
2022-03-01 13:17:37 +0530

    first commit

--- src/test/java/com/sdp3/packages/Sdp3ApplicationTests.java
+++ src/test/java/com/sdp3/packages/Sdp3ApplicationTests.java
@@ -2,8 +2,9 @@ package com.sdp3.packages;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class Sdp3ApplicationTests {
 
 	@Test
